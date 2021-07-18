30ad6b354449c452e1081ed78a0e87202f9a0ba3
Abdullah Adeel <easyautosol@gmail.com>
2021-07-19 00:09:25 +0500

    added server side logic

---
+++ server/server.js
@@ -0,0 +1,55 @@
+require("dotenv").config();
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+
+const socketio = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+
+const io = socketio(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+// Routes and handlers
+app.get("/", (req, res) => {
+  res.redirect("/server-health");
+});
+app.get("/server-health", (req, res) => {
+  res.status(200).send("Server health is okay 👍");
+});
+
+// handling socket connetions for transeferring webrtc data
+io.on("connection", (socket) => {
+  console.log("a user connected with => ", socket.id);
+
+  sockit.emit("me", socket.id);
+
+  // handle socket disconnection
+  socket.on("disconnect", () => {
+    console.log("user disconnected => ", socket.id);
+    socket.broadcast.emit("callended");
+  });
+
+  // Handle calling other user
+  socket.on("calluser", (userToCall, signalData, from, name) => {
+    io.to(userToCall).emit("callUser", signalData, from, name);
+  });
+
+  // Handle Call acceptance
+  socket.on("callaccept", (data) => {
+    io.to(data.to).emit("callaccept", data.signal);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
